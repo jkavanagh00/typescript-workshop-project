87480e6a50c0cf6861732e43478f872bfb15fa68
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-19 19:43:07 +0200

    fix typo in schema

--- src/schemas/cart_items.ts
+++ src/schemas/cart_items.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const CartItemInputData = z.object({
   cart_id: z.coerce.number().int().positive('Cart ID must be a positive integer'),
-  toy_id: z.array(z.coerce.number().int().positive('Toy ID must be a positive integer')).optional(),
+  toy_id: z.coerce.number().int().positive('Toy ID must be a positive integer').optional(),
   quantity: z.coerce.number().int().positive('Quantity must be a positive integer'),
 });
 
