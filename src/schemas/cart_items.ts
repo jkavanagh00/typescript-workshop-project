fd1cda124ab68f735cf213af1310adf8290fa061
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-19 17:46:36 +0200

    add missing quantity property to schema

--- src/schemas/cart_items.ts
+++ src/schemas/cart_items.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 export const CartItemInputData = z.object({
   cart_id: z.coerce.number().int().positive('Cart ID must be a positive integer'),
   toy_id: z.array(z.coerce.number().int().positive('Toy ID must be a positive integer')).optional(),
+  quantity: z.coerce.number().int().positive('Quantity must be a positive integer'),
 });
 
 export const CartItemUpdateData = CartItemInputData.partial();
