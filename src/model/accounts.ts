157e3385b2944316cd780e9fac815f478dc5ea8e
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-19 18:32:49 +0200

    add accounts model

---
+++ src/model/accounts.ts
@@ -0,0 +1,45 @@
+import db from '#config/database';
+const TABLE = 'accounts';
+import { AccountId, AccountInput, AccountUpdate } from '#schemas/types';
+
+function baseQuery(trx = db) {
+  return trx(TABLE);
+}
+
+export async function listAccounts() {
+  const qb = baseQuery().select('*');
+  return qb;
+}
+
+export async function findAccountById(id: AccountId) {
+  const qb = baseQuery().where({ id }).first();
+  return qb;
+}
+
+export async function findAccountByEmail(email: string) {
+  const qb = baseQuery().where({ email }).first();
+  return qb;
+}
+
+export async function createAccount(accountData: AccountInput) {
+  const { name, email, password } = accountData;
+  const newAccount = await baseQuery().insert({ name, email, password }).returning('*');
+  return newAccount[0];
+}
+
+export async function updateAccount(id: AccountId, accountData: AccountUpdate) {
+  const { name, email, password } = accountData;
+  const updatedAccount = await baseQuery()
+    .where({ id })
+    .update({ name, email, password })
+    .returning('*');
+  return updatedAccount[0];
+}
+
+export async function deleteAccount(id: AccountId) {
+  const deletedAccount = await baseQuery()
+    .where({ id })
+    .delete()
+    .returning('*');
+  return deletedAccount[0];
+}
