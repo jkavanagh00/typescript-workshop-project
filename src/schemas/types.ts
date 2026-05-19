fa32a7393fdce748d081b96484960ce10890784d
jkavanagh00 <jkavanagh00@gmail.com>
2026-05-19 18:32:20 +0200

    add schemas and types for auth

--- src/schemas/types.ts
+++ src/schemas/types.ts
@@ -2,7 +2,9 @@ import { z } from 'zod';
 import { AccountIdParam, AccountInputData, AccountUpdateData } from './accounts';
 import { CartIdParam, CartInputData, CartUpdateData } from './carts';
 import { CartItemIdParam, CartItemInputData, CartItemUpdateData } from './cart_items';
+import { LoginInputData, RegisterInputData } from './auth';
 import { ToyIdParam, ToyInputData, ToyUpdateData } from './toys';
+import exp from 'constants';
 
 export type AccountInput = z.infer<typeof AccountInputData>;
 export type AccountUpdate = z.infer<typeof AccountUpdateData>;
@@ -15,6 +17,9 @@ export type CartId = z.infer<typeof CartIdParam>;
 export type CartItemInput = z.infer<typeof CartItemInputData>;
 export type CartItemUpdate = z.infer<typeof CartItemUpdateData>;
 export type CartItemId = z.infer<typeof CartItemIdParam>;
+
+export type LoginInput = z.infer<typeof LoginInputData>;
+export type RegisterInput = z.infer<typeof RegisterInputData>;
 
 export type ToyInput = z.infer<typeof ToyInputData>;
 export type ToyUpdate = z.infer<typeof ToyUpdateData>;
