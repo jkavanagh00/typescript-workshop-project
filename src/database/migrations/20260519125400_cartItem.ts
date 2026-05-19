1f6cbbcf6c2cc25ee21c9a6cbd16df482272ba06
irynalopatina <lopatina.iryna@gmail.com>
2026-05-19 13:16:35 +0200

    add cartItem migration file

---
+++ src/database/migrations/20260519125400_cartItem.ts
@@ -0,0 +1,15 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('cartItem', (t) => {
+    t.increments('id').primary;
+    t.integer('toy_id').notNullable().references('id').inTable('toy');
+    t.integer('cart_id').notNullable().references('id').inTable('cart');
+    t.integer('quantity').notNullable();
+    t.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTableIfExists('cartItem');
+}
