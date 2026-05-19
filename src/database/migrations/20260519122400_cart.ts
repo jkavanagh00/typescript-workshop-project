5f9bf68dbd7db82e9e8bc72049a083ffd7b08ee5
Sayyed Wasim Hussain Bukhari <wasimhussain3@gmail.com>
2026-05-19 12:36:05 +0200

    add cart table migration file

---
+++ src/database/migrations/20260519122400_cart.ts
@@ -0,0 +1,15 @@
+import knex from 'knex';
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex) {
+  await knex.schema.createTable('cart', (t) => {
+    t.increments('id').primary;
+    t.integer('account_id');
+    t.enu('status', ['active', 'checked_out', 'abandoned']);
+    t.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex) {
+  await knex.schema.dropTableIfExists('cart');
+}
