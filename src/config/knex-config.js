8960d7a69475bc86e4166d700c4b8ed31f982ad0
Sayyed Wasim Hussain Bukhari <wasimhussain3@gmail.com>
2026-05-19 13:29:07 +0200

    update knex config for SQLite

--- src/config/knex-config.js
+++ src/config/knex-config.js
@@ -1,28 +1,32 @@
 const client = process.env.DB_CLIENT ?? "sqlite3";
-const defaultDbFilename =
-    client === "sqlite3" ? "src/db/database.sqlite" : null;
+const defaultDbFilename = "src/database/dev.sqlite3";
 
 export function createKnexConfig() {
-    return {
-        client,
-        connection: {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE_NAME,
-            filename: process.env.DB_SQLITE_FILENAME ?? defaultDbFilename,
-            ssl:
-                process.env.DB_USE_SSL === "true"
-                    ? { rejectUnauthorized: false }
-                    : false,
+  const isSqlite = client === "sqlite3";
+
+  return {
+    client,
+    connection: isSqlite
+      ? {
+          filename: process.env.DB_SQLITE_FILENAME ?? defaultDbFilename,
+        }
+      : {
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE_NAME,
+          ssl:
+            process.env.DB_USE_SSL === "true"
+              ? { rejectUnauthorized: false }
+              : false,
         },
-        useNullAsDefault: process.env.DB_USE_NULL_AS_DEFAULT === "true",
-        migrations: {
-            directory: "src/db/migrations",
-        },
-        seeds: {
-            directory: "src/db/seeds",
-        },
-    };
+    useNullAsDefault: isSqlite || process.env.DB_USE_NULL_AS_DEFAULT === "true",
+    migrations: {
+      directory: "src/database/migrations",
+    },
+    seeds: {
+      directory: "src/database/seeds",
+    },
+  };
 }
