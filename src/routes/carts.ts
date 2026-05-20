5751e4eada0306db599c7f0c9342df8ae50b5910
irynalopatina <lopatina.iryna@gmail.com>
2026-05-20 19:27:52 +0200

    feat: protect cart routes

--- src/routes/carts.ts
+++ src/routes/carts.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { getOwnCart, addCart, addItemToCart, removeItemFromCart } from "#controllers/carts";
 import toysRouter from "./toys";
+import { authenticate } from "../middleware/authenticate";
 
 const cartsRouter = express.Router();
 
-cartsRouter.get('/', getOwnCart);
-cartsRouter.post('/', addCart);
-cartsRouter.post('/items', addItemToCart);
-cartsRouter.delete('/items/:id', removeItemFromCart);
+cartsRouter.get('/', authenticate, getOwnCart);
+cartsRouter.post('/', authenticate, addCart);
+cartsRouter.post('/items', authenticate, addItemToCart);
+cartsRouter.delete('/items/:id', authenticate, removeItemFromCart);
 
 export default cartsRouter;
